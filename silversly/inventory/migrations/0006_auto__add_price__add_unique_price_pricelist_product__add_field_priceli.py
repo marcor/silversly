70737c15b9bca9f725c76365bdddcd8745b72088
Marco Rimoldi <marcor@users.noreply.github.com>
2011-02-19 14:41:04 +0000

    Some changes needed to migrate the existing database to the new Price model.

---
+++ silversly/inventory/migrations/0006_auto__add_price__add_unique_price_pricelist_product__add_field_priceli.py
@@ -0,0 +1,193 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Price'
+        db.create_table('inventory_price', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('method', self.gf('django.db.models.fields.CharField')(max_length=2)),
+            ('value', self.gf('django.db.models.fields.DecimalField')(max_digits=7, decimal_places=2)),
+            ('markup', self.gf('django.db.models.fields.PositiveSmallIntegerField')()),
+            ('pricelist', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['inventory.Pricelist'])),
+            ('product', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['inventory.Product'])),
+        ))
+        db.send_create_signal('inventory', ['Price'])
+
+        # Adding unique constraint on 'Price', fields ['pricelist', 'product']
+        db.create_unique('inventory_price', ['pricelist_id', 'product_id'])
+
+        # Adding field 'Pricelist.default_method'
+        db.add_column('inventory_pricelist', 'default_method', self.gf('django.db.models.fields.CharField')(default='%=', max_length=2), keep_default=False)
+
+
+    def backwards(self, orm):
+        
+        # Removing unique constraint on 'Price', fields ['pricelist', 'product']
+        db.delete_unique('inventory_price', ['pricelist_id', 'product_id'])
+
+        # Deleting model 'Price'
+        db.delete_table('inventory_price')
+
+        # Deleting field 'Pricelist.default_method'
+        db.delete_column('inventory_pricelist', 'default_method')
+
+
+    models = {
+        'inventory.address': {
+            'Meta': {'object_name': 'Address'},
+            'city': ('django.db.models.fields.CharField', [], {'max_length': '30'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'postcode': ('django.db.models.fields.CharField', [], {'max_length': '5'}),
+            'province': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'street': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'inventory.bank': {
+            'Meta': {'object_name': 'Bank'},
+            'abi': ('django.db.models.fields.CharField', [], {'max_length': '5'}),
+            'cab': ('django.db.models.fields.CharField', [], {'max_length': '5'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'inventory.cart': {
+            'Meta': {'ordering': "['date']", 'object_name': 'Cart'},
+            'current': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'})
+        },
+        'inventory.cartitem': {
+            'Meta': {'object_name': 'CartItem'},
+            'cart': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.Cart']"}),
+            'discount': ('django.db.models.fields.PositiveSmallIntegerField', [], {'default': '0'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.Product']"}),
+            'quantity': ('django.db.models.fields.DecimalField', [], {'max_digits': '7', 'decimal_places': '2'}),
+            'update': ('django.db.models.fields.BooleanField', [], {'default': 'True'})
+        },
+        'inventory.category': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Category'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '60'}),
+            'parent': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.Category']", 'null': 'True', 'blank': 'True'})
+        },
+        'inventory.customer': {
+            'Meta': {'object_name': 'Customer'},
+            'bank': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['inventory.Bank']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '20'}),
+            'costs': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '7', 'decimal_places': '2'}),
+            'email': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '30', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'main_address': ('django.db.models.fields.TextField', [], {}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'payment_method': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'phone': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '15', 'blank': 'True'}),
+            'shipping_address': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'inventory.ddt': {
+            'Meta': {'ordering': "['date', 'number']", 'object_name': 'Ddt'},
+            'cart': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.Cart']"}),
+            'customer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.RetailCustomer']", 'null': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.DateField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'main_address': ('django.db.models.fields.TextField', [], {}),
+            'number': ('django.db.models.fields.PositiveSmallIntegerField', [], {'unique': 'True'}),
+            'shipping_address': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'shipping_date': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2011, 2, 19, 15, 22, 37, 741000)'})
+        },
+        'inventory.invoice': {
+            'Meta': {'object_name': 'Invoice'},
+            'bank': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['inventory.Bank']", 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'costs': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '7', 'decimal_places': '2'}),
+            'ddts': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['inventory.Ddt']", 'symmetrical': 'False'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'number': ('django.db.models.fields.PositiveSmallIntegerField', [], {'unique': 'True'}),
+            'payment_method': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'inventory.logentry': {
+            'Meta': {'object_name': 'LogEntry'},
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'price': ('django.db.models.fields.DecimalField', [], {'null': 'True', 'max_digits': '8', 'decimal_places': '3'}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.Product']"}),
+            'quantity': ('django.db.models.fields.DecimalField', [], {'max_digits': '8', 'decimal_places': '3'}),
+            'supplier': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.Supplier']", 'null': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '1'})
+        },
+        'inventory.markup': {
+            'Meta': {'unique_together': "(('pricelist', 'product'),)", 'object_name': 'Markup'},
+            'charge': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'pricelist': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.Pricelist']"}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.Product']"})
+        },
+        'inventory.price': {
+            'Meta': {'unique_together': "(('pricelist', 'product'),)", 'object_name': 'Price'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'markup': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'method': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'pricelist': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.Pricelist']"}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.Product']"}),
+            'value': ('django.db.models.fields.DecimalField', [], {'max_digits': '7', 'decimal_places': '2'})
+        },
+        'inventory.pricelist': {
+            'Meta': {'object_name': 'Pricelist'},
+            'default_markup': ('django.db.models.fields.PositiveSmallIntegerField', [], {}),
+            'default_method': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '25', 'primary_key': 'True'}),
+            'rounding': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        },
+        'inventory.product': {
+            'Meta': {'object_name': 'Product'},
+            'base_price': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '8', 'decimal_places': '3'}),
+            'category': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.Category']"}),
+            'code': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '13'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'markups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['inventory.Pricelist']", 'null': 'True', 'through': "orm['inventory.Markup']", 'symmetrical': 'False'}),
+            'min_quantity': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '8', 'decimal_places': '3'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '60'}),
+            'prices': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "'products2'", 'null': 'True', 'through': "orm['inventory.Price']", 'to': "orm['inventory.Pricelist']"}),
+            'quantity': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '8', 'decimal_places': '3'}),
+            'suppliers': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['inventory.Supplier']", 'null': 'True', 'through': "orm['inventory.Supply']", 'symmetrical': 'False'}),
+            'unit': ('django.db.models.fields.CharField', [], {'max_length': '15'})
+        },
+        'inventory.receipt': {
+            'Meta': {'object_name': 'Receipt'},
+            'cart': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.Cart']"}),
+            'customer': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.RetailCustomer']", 'null': 'True', 'blank': 'True'}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'number': ('django.db.models.fields.PositiveIntegerField', [], {})
+        },
+        'inventory.retailcustomer': {
+            'Meta': {'object_name': 'RetailCustomer'},
+            'due': ('django.db.models.fields.DecimalField', [], {'default': '0', 'max_digits': '8', 'decimal_places': '3'}),
+            'email': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '30', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'phone': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '15', 'blank': 'True'})
+        },
+        'inventory.supplier': {
+            'Meta': {'object_name': 'Supplier'},
+            'email': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '30', 'blank': 'True'}),
+            'fax': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '15', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '50'}),
+            'phone': ('django.db.models.fields.CharField', [], {'default': 'None', 'max_length': '15', 'blank': 'True'})
+        },
+        'inventory.supply': {
+            'Meta': {'unique_together': "(('product', 'supplier'),)", 'object_name': 'Supply'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '20', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'price': ('django.db.models.fields.DecimalField', [], {'max_digits': '8', 'decimal_places': '3'}),
+            'product': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.Product']"}),
+            'supplier': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['inventory.Supplier']"}),
+            'updated': ('django.db.models.fields.DateField', [], {'auto_now': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['inventory']
