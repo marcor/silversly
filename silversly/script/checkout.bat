14ef9579077982f679ff482fa8139bd7b6f18be1
Marco Rimoldi <marcor@users.noreply.github.com>
2011-07-11 09:30:53 +0000

    Fixed xcopy option.

--- silversly/script/checkout.bat
+++ silversly/script/checkout.bat
@@ -3,6 +3,6 @@ fossil open %2
 
 fossil pull
 fossil co "%3" --force
-xcopy "%4\*.*" "%5" /D /Y  
+xcopy "%4\*.*" "%5" /D /Y /S 
 
 copy /b "%6" +,,
--- silversly/settings.py
+++ silversly/settings.py
@@ -1,6 +1,6 @@
 import sys, os
 
-VERSION = "0.6"
+VERSION = "0.5"
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(PROJECT_DIR)
