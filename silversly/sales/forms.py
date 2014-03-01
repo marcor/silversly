4cfadb5f38d47e8206a211549aee3b35e2913e9c
Marco Rimoldi <marcor@users.noreply.github.com>
2014-03-01 13:42:22 +0000

    Fixed validation of invoice forms at new year

--- silversly/sales/forms.py
+++ silversly/sales/forms.py
@@ -32,10 +32,22 @@ class InvoiceForm(ModelForm):
         model = Invoice
         fields = ("number", "date", "payment_method", "costs")
     
+    def clean(self):
+        cleaned_data = super(InvoiceForm, self).clean()
+        date = cleaned_data.get("date")
+        number = cleaned_data.get("number")
+        if date and number and self.prev_invoice:
+            # late invoice (belongs to last year)  
+            if date.year == self.prev_invoice.date.year and number <= self.prev_invoice.number:
+                msg = "Il primo numero disponibile è %d" % (self.prev_invoice.number + 1,)                
+                self._errors["number"] = self.error_class([msg])
+                del cleaned_data["number"]
+        return cleaned_data
+        
     def clean_number(self):
         number = self.cleaned_data['number']
-        if self.prev_invoice and number <= self.prev_invoice.number:
-                raise ValidationError("Il primo numero disponibile è %d" % (self.prev_invoice.number + 1,))
+        if number < 1:
+                raise ValidationError("Dai là, ma che numero è?")
         return number
     
     def clean_date(self):
