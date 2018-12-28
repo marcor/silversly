0bd103072c3c95f8d8da2f8579203b10a5a16ea0
Marco Rimoldi <marcor@users.noreply.github.com>
2018-12-28 01:12:52 +0000

    Working Bank form fields!

--- silversly/people/forms.py
+++ silversly/people/forms.py
@@ -1,8 +1,60 @@
 from django.forms import *
 from models import *
 import re
+from django.utils.translation import ugettext_lazy as _
+from django.core import validators
+from django.utils.safestring import mark_safe
+
 
 ADDRESS_FORMAT = getattr(settings, 'ADDRESS_FORMAT')
+
+class BankWidget(MultiWidget):
+    def __init__(self, attrs=None):
+        widgets = (TextInput(attrs=attrs),
+                   TextInput(attrs=attrs))
+        super(BankWidget, self).__init__(widgets, attrs)
+
+    def decompress(self, value):
+        if value:
+            bank = Bank.objects.get(pk=value)
+            return [bank.abi, bank.cab]
+        return [None, None]
+
+    def format_output(self, rendered_widgets):
+        # hack
+        rendered_widgets.insert(2, '<label for="id_bank_1">CAB:</label>')
+        return u''.join(rendered_widgets)
+
+class BankSplitField(MultiValueField):
+    widget = BankWidget
+    default_error_messages = {
+        'invalid_abi': _(u'ABI non valido.'),
+        'invalid_cab': _(u'CAB non valido.'),
+    }
+
+    def __init__(self, *args, **kwargs):
+        errors = self.default_error_messages.copy()
+        if 'error_messages' in kwargs:
+            errors.update(kwargs['error_messages'])
+        fields = (
+            CharField(label="ABI", max_length=5, min_length=5, error_messages={'invalid': errors['invalid_abi']}),
+            CharField(label="CAB", max_length=5, min_length=5, error_messages={'invalid': errors['invalid_cab']}))
+        super(BankSplitField, self).__init__(fields, *args, **kwargs)
+
+    def compress(self, data_list):
+        if data_list:
+            # Raise a validation error if time or date is empty
+            # (possible if SplitDateTimeField has required=False).
+            if data_list[0] in validators.EMPTY_VALUES:
+                raise ValidationError(self.error_messages['invalid_abi'])
+            if data_list[1] in validators.EMPTY_VALUES:
+                raise ValidationError(self.error_messages['invalid_cab'])
+            try:
+                result = Bank.objects.get(abi=data_list[0], cab=data_list[1])
+            except:
+                result = Bank.objects.create(abi=data_list[0], cab=data_list[1]).pk
+            return result
+        return None
 
 class SupplierForm(ModelForm):
     class Meta:
@@ -56,9 +108,11 @@ class PAInfoForm(CompanyInfoForm):
         fields = ("name", "cf", "piva", "sdi_code", "phone", "email", "main_address")
 
 class CompanyCommercialForm(ModelForm):
+    bank = BankSplitField(label="ABI", required=False)  # hack
+
     class Meta:
         model = CompanyCustomer
-        fields = ("due", "pricelist", "discount", "payment_method", "shipping_address")
+        fields = ("due", "pricelist", "discount", "payment_method", "bank", "shipping_address")
 
 class CompanyQuickForm(ModelForm):
     class Meta:
